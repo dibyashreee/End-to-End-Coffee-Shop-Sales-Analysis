f073a51b8f24c2891a7c9e83803556a1105eafa7
dibyashreee <145294589+dibyashreee@users.noreply.github.com>
2026-03-27 00:05:30 +0530

    Delete sql/02_data_import.sql

--- sql/02_data_import.sql
+++
@@ -1,7 +0,0 @@
-COPY coffee_sales
-FROM 'C:\project 1\working data demo.csv'
-DELIMITER ',' CSV HEADER;
-
-/*Imports raw sales data from a local CSV file into the 'coffee_sales' staging table.
-Note: Uses the COPY command to efficiently load bulk data, skipping the header row to ensure only record data is processed.
-*/
